fix: remove normalization and tensor transformation

autoalbument/faster_autoaugment/models/policy_model.py
@@ -173,11 +173,12 @@ class Policy(nn.Module):
             [
                 *preprocessing_transforms,
                 OneOf([sp.create_transform(input_dtype, p=sub_policy_p) for sp in self.sub_policies], p=1),
-                A.Normalize(
-                    mean=self._mean.tolist(),
-                    std=self._std.tolist(),
-                    max_pixel_value=MAX_VALUES_BY_INPUT_DTYPE[input_dtype],
-                ),
-                ToTensorV2(transpose_mask=True),
+               # A.Normalize(
+                #    mean=self._mean.tolist(),
+                 #   std=self._std.tolist(),
+                  #  max_pixel_value=MAX_VALUES_BY_INPUT_DTYPE[input_dtype],
+                #),
+                #ToTensorV2(transpose_mask=True),
             ]
         )
+